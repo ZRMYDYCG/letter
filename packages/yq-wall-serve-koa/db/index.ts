b1ae97e3a1deb18e9676d33a0e1f638916d60531
ZRMYDYCG <547471919@qq.com>
2024-11-20 00:03:19 +0800

    chore: Connect to the MongoDB database

---
+++ packages/yq-wall-serve-koa/db/index.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import type { ConnectOptions } from "mongoose";
+
+class Database {
+    private readonly connectionString: string;
+    private readonly options: ConnectOptions;
+
+    constructor() {
+        this.connectionString = 'mongodb://localhost:27017/messages-wall'
+        this.options = {}
+    }
+
+    async connect(): Promise<void> {
+        try {
+            await mongoose.connect(this.connectionString, this.options);
+            console.log("Connected to MongoDB");
+        } catch (err) {
+            console.log("Error connecting to MongoDB:", err.message);
+        }
+    }
+}
+
+export default new Database()
--- packages/yq-wall-serve-koa/src/index.ts
+++ packages/yq-wall-serve-koa/src/index.ts
@@ -1,10 +1,16 @@
-import Koa from 'koa'
+import Koa from 'koa';
+import Database from '../db/index'
 
 const app = new Koa()
 const msg: string = 'Hello World'
+
+// 连接数据库
+Database.connect()
 
 app.use(async (ctx: Koa.Context): Promise<void> => {
     ctx.body = msg
 })
 
-app.listen(7000)
+app.listen(5173, () => {
+    console.log("Server is running on http://localhost:5173");
+})
