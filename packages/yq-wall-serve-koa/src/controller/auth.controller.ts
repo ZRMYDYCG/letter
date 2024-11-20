75b4252fa59e23f7002f1231ca271fb7804859a4
ZRMYDYCG <547471919@qq.com>
2024-11-20 22:09:40 +0800

    feat: User registration and duplicate verification

--- packages/yq-wall-serve-koa/src/controller/auth.controller.ts
+++ packages/yq-wall-serve-koa/src/controller/auth.controller.ts
@@ -1,5 +1,6 @@
 import type { Context } from 'koa'
 import authService from '../service/auth.service'
+import userModel from "../models/users.model";
 
 class authController {
     async login(ctx: Context) {
@@ -10,6 +11,43 @@ class authController {
             message: '登录成功',
             data: { token, user }
         }
+    }
+    async register(ctx: Context) {
+        const { username, password } = ctx.request.body as { username: string, password: string }
+
+        // 查询当前的用户注册的用户名是否存在
+        const user = await userModel.findOne({ username })
+
+        if(user) {
+            ctx.body = {
+                code: 500,
+                message: '该用户名已被注册',
+            }
+            return
+        }
+
+
+        await userModel.create({username, password}).then((res) => {
+            if(res) {
+                ctx.body = {
+                    code: 200,
+                    message: '注册成功',
+                    data: res,
+                }
+            } else {
+                ctx.body = {
+                    code: 300,
+                    message: '注册失败',
+                }
+            }
+        }).catch((err) => {
+            ctx.body = {
+                code: 500,
+                message: '注册出现异常',
+                data: err,
+            }
+        })
+
     }
 }
 
--- packages/yq-wall-serve-koa/src/router/auth.router.ts
+++ packages/yq-wall-serve-koa/src/router/auth.router.ts
@@ -5,7 +5,15 @@ const authRouter = new Router({
     prefix: '/auth'
 })
 
+/**
+ * @desc 用户登录
+ * */
 authRouter.post('/login', authController.login)
+
+/**
+ * @desc 用户注册
+ * */
+authRouter.post('/register', authController.register)
 
 
 export default authRouter
