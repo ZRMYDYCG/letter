22335ecce0794caa813525a7c64c94d0405722d9
ZRMYDYCG <547471919@qq.com>
2024-11-22 21:06:57 +0800

    feat: Add a comment entity

---
+++ packages/yq-wall-serve-koa/src/models/comment.model.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const commentSchema = new mongoose.Schema({
+    /* 评论用户ID */
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    /* 当前评论关联的留言ID */
+    messageId: { type: mongoose.Schema.Types.ObjectId, ref: 'messages', required: true },
+    /* 评论内容 */
+    content: { type: String, required: true },
+    /* 评论时间 */
+    createdAt: { type: Date, default: Date.now }
+})
+
+const commentModel = mongoose.model("comments", commentSchema)
+
+export default commentModel
--- packages/yq-wall-serve-koa/src/models/message.model.ts
+++ packages/yq-wall-serve-koa/src/models/message.model.ts
@@ -1,11 +1,25 @@
 import mongoose from "mongoose"
 
 const messageSchema = new mongoose.Schema({
+    /*留言用户ID*/
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    /*留言用户签名*/
+    nickName: { type: String, default: "" },
+    /*留言内容*/
     content: { type: String, required: true },
+    /*留言类型*/
     type: { type: String, required: true },
+    /*背景色*/
     color: { type: String, required: true },
+    /*背景图*/
+    image: { type: String, default: "" },
+    /*标签*/
     tag: { type: String, required: true },
+    /*点赞数*/
+    like: { type: Number, default: 0 },
+    /*评论数*/
+    comment: { type: Number, default: 0 },
+    /*留言发布时间*/
     createdAt: { type: Date, default: Date.now }
 })
 
