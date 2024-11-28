082687eae65d25037dae9d3b5a12ddc3586d518c
ZRMYDYCG <547471919@qq.com>
2024-11-29 00:46:30 +0800

    feat: Optimize the cropped image

--- packages/yq-wall-spa/src/views/home/children/components/message-text-card/card-for-canves.vue
+++ packages/yq-wall-spa/src/views/home/children/components/message-text-card/card-for-canves.vue
@@ -8,19 +8,19 @@ interface IProps {
 
 const props = defineProps<IProps>()
 const emits = defineEmits(['item-click'])
-
 </script>
 
 <template>
-  <div class="yi-card" :style="{ width: width, background: cardColor[note.color] }">
-    <div class="flex justify-between items-center p-[10px] border-b border-gray-300">
-      <img class="w-[30px] h-[30px] border rounded-full" src="@/assets/images/logo.svg" alt="#">
-      <span class="text-gray-500 text-sm">{{ label[0][note.tag] }}</span>
+  <div class="h-[240px] flex flex-col justify-between" :style="{ width: width, background: cardColor[note.color], fontFamily: 'XiaoKeNaiNao' }">
+    <div class="flex justify-between items-center border-b border-gray-300 h-[40px] px-[10px]">
+      <img class="w-[30px] h-[30px] border rounded-full" src="@/assets/images/logo.svg" alt="#" />
+      <div class="text-gray-500 flex items-center h-full">
+        <img src="@/assets/images/tag.svg"  class="w-[12px] h-[12px] mr-[5px]" alt="#" />
+        <div class="pb-[15px]">{{label[0][note.tag]}}</div>
+      </div>
     </div>
-    <p class="message">{{ note.content }}</p>
-    <div class="footer">
-      <div class="name">{{ note.nickName || '匿名' }}</div>
-    </div>
+    <p class="block h-[140px] py-[5px] px-[20px] text-[14px] text-[#202020]">{{ note.content }}</p>
+    <div class="flex justify-end border-b border-gray-300 p-[10px]">{{ note.nickName || '匿名' }}</div>
   </div>
 </template>
 
@@ -28,36 +28,5 @@ const emits = defineEmits(['item-click'])
 @font-face {
   font-family: XiaoKeNaiNao;
   src: url("@/assets/fonts/xknn.ttf");
-}
-.yi-card {
-  height: 240px;
-  padding: 10px 20px 16px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  cursor: pointer;
-}
-.yi-card .header {
-  display: flex;
-  justify-content: space-between;
-  padding-bottom: 16px;
-}
-
-.yi-card .message {
-  height: 140px;
-  font-family: XiaoKeNaiNao;
-  font-size: 14px;
-  color: #202020;
-}
-.yi-card .footer {
-  display: flex;
-  justify-content: space-between;
-}
-
-.yi-card .footer .name {
-  font-family: XiaoKeNaiNao;
-  font-size: 16px;
-  color: #202020;
-  font-weight: 700;
 }
 </style>
