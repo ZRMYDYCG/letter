f539b6d3e7d9d5f80137767d324d1d10e754d3e5
ZRMYDYCG <547471919@qq.com>
2024-12-08 13:44:56 +0800

    feat: (#14) 图片下载出现破损的情况

--- packages/yq-wall-serve-koa/src/router/download.router.ts
+++ packages/yq-wall-serve-koa/src/router/download.router.ts
@@ -4,15 +4,18 @@ import send from "koa-send";
 const downloadRouter = new Router();
 
 /**
- * @dencription 文件下载
- * */
-downloadRouter.post("/download/:filename", async (ctx: any) => {
+ * @description 文件下载
+ */
+downloadRouter.post("/download/:filename", async (ctx) => {
   const filename = ctx.params.filename;
-
-  console.log(`download ${filename}`);
   const filePath = `./src/public/uploads/${filename}`;
-  ctx.attachment(filePath);
-  ctx.body = await send(ctx, filePath);
+
+  try {
+    await send(ctx, filePath);
+  } catch (error) {
+    ctx.status = 404;
+    ctx.body = { message: "文件未找到" };
+  }
 });
 
 export default downloadRouter;
